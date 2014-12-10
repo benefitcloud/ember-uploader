1e1db5b888f64f7d8aa7e8dcd2723cd22fd9d5f5
Joshua Borton <digitaltoad@gmail.com>
2014-12-10 15:27:39 -0500

    Simplify main export

--- packages/ember-uploader/lib/main.js
+++ packages/ember-uploader/lib/main.js
@@ -2,9 +2,8 @@ import Uploader from 'ember-uploader/uploader';
 import S3Uploader from 'ember-uploader/s3';
 import FileField from 'ember-uploader/file-field';
 
-var EmberUploader = {};
-EmberUploader.Uploader = Uploader;
-EmberUploader.S3Uploader = S3Uploader;
-EmberUploader.FileField = FileField;
-
-export default EmberUploader;
+export default {
+  Uploader: Uploader,
+  S3Uploader: S3Uploader,
+  FileField: FileField
+};
