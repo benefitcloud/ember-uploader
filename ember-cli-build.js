eb9d27b568e1700657b621ce10bda4f915c80c17
snewcomer <snewcomer24@gmail.com>
2018-05-18 14:08:28 -0600

    update to 2.18

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,44 +1,19 @@
-/* eslint-disable */
-var path = require('path');
-var jsStringEscape = require('js-string-escape');
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+'use strict';
 
-function render(errors) {
-  if (!errors) { return ''; }
-  return errors.map(function(error) {
-    return error.line + ':' + error.column + ' ' +
-      ' - ' + error.message + ' (' + error.ruleId +')';
-  }).join('\n');
-}
-
-// Qunit test generator
-function eslintTestGenerator(relativePath, errors) {
-  var pass = !errors || errors.length === 0;
-  return "import { module, test } from 'qunit';\n" +
-    "module('ESLint - " + path.dirname(relativePath) + "');\n" +
-    "test('" + relativePath + " should pass ESLint', function(assert) {\n" +
-    "  assert.ok(" + pass + ", '" + relativePath + " should pass ESLint." +
-    jsStringEscape("\n" + render(errors)) + "');\n" +
-   "});\n";
-}
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    hinting: false,
-    "ember-cli-qunit": {
-      useLintTree: false
-    },
-    eslint: {
-      testGenerator: eslintTestGenerator
-    },
+  let app = new EmberAddon(defaults, {
     babel: {
-      includePolyfill: true
+      includePolyfill: true,
+      plugins: [
+        'transform-object-rest-spread'
+      ]
     }
-    // Add options here
   });
 
   /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
@@ -46,4 +21,3 @@ module.exports = function(defaults) {
 
   return app.toTree();
 };
-/* eslint-enable */
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-/* eslint-disable */
 'use strict';
 
 module.exports = {
-  name: 'ember-uploader',
-  treeForAddon: function(dir) {
-    var version      = require('./lib/version');
-    var merge        = require('broccoli-merge-trees');
-
-    return this._super.treeForAddon.call(this, merge([version(), dir]));
-  }
+  name: 'ember-uploader'
 };
-/* eslint-enable */
