54c8f7675a41599c3d9fe11d7b3cfd945032ba41
snewcomer <snewcomer24@gmail.com>
2018-06-01 18:33:00 -0700

    update based on feedback

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,13 +3,7 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
-    babel: {
-      plugins: [
-        'transform-object-rest-spread'
-      ]
-    }
-  });
+  let app = new EmberAddon(defaults);
 
   /*
     This build file specifies the options for the dummy test app of this
