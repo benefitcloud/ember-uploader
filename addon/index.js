3d07d3c5d2d45b1b2ccca2eaa692d3369aafcb0c
Scott Newcomer <snewcomer@users.noreply.github.com>
2018-06-08 10:22:48 -0700

    remove version checker (#176)

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,3 @@
-import { VERSION } from '@ember/version';
 import Ember from 'ember';
 
 /**
@@ -8,12 +7,6 @@ import Ember from 'ember';
 */
 
 import EmberUploader from 'ember-uploader/core';
-
-if (VERSION.match(/^1/)) {
-  Ember.Logger.warn(
-    'This version of Ember Uploader has not been tested on Ember 1.x. Use at your own risk.');
-}
-
 import { Uploader, S3Uploader } from 'ember-uploader/uploaders';
 
 EmberUploader.Uploader   = Uploader;
