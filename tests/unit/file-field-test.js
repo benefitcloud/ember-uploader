a43aab5898ce0c407a23fee25fa82412a09d7707
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2023-09-05 13:48:10 +0600

    Fix FileField test

--- tests/unit/file-field-test.js
+++ tests/unit/file-field-test.js
@@ -1,21 +1,22 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, triggerEvent } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+// import { triggerEvent } from '../../dist/assets/test-support';
 
 module('EmberUploader.FileField', function(hooks) {
-  setupTest(hooks);
+  setupRenderingTest(hooks);
 
-  test('it triggers `filesDidChange` on change', function(assert) {
-    let result;
+  test('it triggers `filesDidChange` on change', async function(assert) {
     assert.expect(1);
 
-    const fileField = this.owner.factoryFor('component:file-field').create({
-      filesDidChange (files) {
-        result = files;
-      }
+    this.set('filesDidChange', (actual) => {
+      let expected = ['foo'];
+      assert.deepEqual(actual, expected, 'submitted value is passed to external action');
     });
 
-    fileField.change({ target: { files: [ 'foo' ] }});
-
-    assert.deepEqual(result, ['foo'], 'it returns the files that changed');
+    await render(hbs`<FileField @filesDidChange={{this.filesDidChange}} />`);
+    let inputElement = this.element.querySelector('input[type="file"]');
+    await triggerEvent(inputElement, 'change', { files: ['foo'] });
   });
 });
